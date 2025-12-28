1a7cda306404ee40ca4f7bf4c07df6aa82929364
Heshith <heshithdashan24@gmail.com>
2025-12-28 13:28:51 +0530

    Update

--- src/possystem/PosSystem.java
+++ src/possystem/PosSystem.java
@@ -10,27 +10,23 @@ public class PosSystem {
 
     public static void main(String[] args) {
         try {
-            // 1. Theme එක Setup කිරීම
+
             FlatDarkLaf.setup();
 
-            // 2. Font එක Load කරගැනීම
             InputStream fontStream = PosSystem.class.getResourceAsStream("/fonts/Poppins-Regular.ttf");
             
             if (fontStream != null) {
-                // Font එක හදලා size එක 14 දානවා
+
                 Font poppins = Font.createFont(Font.TRUETYPE_FONT, fontStream).deriveFont(14f); 
                 
-                // System එකට Register කරනවා
                 GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
                 ge.registerFont(poppins);
                 
-                // මුළු App එකටම මේ ෆොන්ට් එක දාන්න කියනවා
                 UIManager.put("defaultFont", poppins);
             } else {
                 System.out.println("⚠️ Font file not found!");
             }
             
-            // 3. Login Form එක Open කිරීම
             new LoginForm().setVisible(true);
             
         } catch (Exception e) {
